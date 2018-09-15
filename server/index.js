95a795036febffe493ca3e3ed7de8f7ff690ee81
Jen <demievillecodes@gmail.com>
2018-09-15 21:44:35 +0000

    Routes are working and configured to mlab

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ const express = require("express"),
       methodOverride = require('method-override'),
       mongoose = require('mongoose'),
       User = require('./Models/User'),
-      Posters = require('.Models/Posters');
+      Posters = require('./Models/Posters');
       
+      require('dotenv').config();
       app.use(bodyParser.urlencoded({extended: true}));
+      app.use(bodyParser.json());
       app.use(express.static("public"));
       app.use(methodOverride("_method"));
       
+
 
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
@@ -17,7 +20,9 @@ app.use((req, res, next) => {
    next();
 });
 
-app.get('/', (req, res) => {
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
+
+app.get('/posters', (req, res) => {
     Posters.find({}, (err, posters) => {
         if(err) {
             console.log("There is an error yo", err);
@@ -28,26 +33,42 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/posters', (req, res) => {
-    let posterObject = JSON.parse(req);
+app.post('/posters/new', (req, res) => {
+    let posterObject = req.body;
     let newPoster = {name: posterObject.name, project: posterObject.project, rating: posterObject.rating, picture: posterObject.picture};
     Posters.create(newPoster, (err, created) => {
         if(err) {
             console.log(err);
         } else {
+            res.json(newPoster)
             console.log("New poster added!");
         }
     });
 });
 
-app.post('/user', (req, res) => {
-    let userObject = JSON.parse(req);
+app.get('/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if(err) {
+            console.log("There is an error yo", err);
+            res.json({ err: "There is an error yo"});
+        } else {
+            res.json(users);
+        }
+    });
+});
+
+app.post('/users/new', (req, res) => {
+    let userObject = req.body;
     let newUser = {username: userObject.username, password: userObject.password, rating: userObject.rating, github: userObject.github, picture: userObject.picture};
     User.create(newUser, (err, created) => {
        if(err) {
            console.log(err);
+           res.json({ err: "There is an error yo"});
        } else {
+           res.json(newUser);
            console.log("New user added!");
        }
     });
 });
+
+app.listen(process.env.PORT, process.env.IP, () => console.log(`running port on: ${process.env.PORT}`));

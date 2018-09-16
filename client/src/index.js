7fd7693c3b50644dfcaa21eb6872d4ef1c6235f8
Brarian <b.namonywa@gmail.com>
2018-09-16 10:16:20 -0500

    daniel's nav added

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Listing from './listing.js';
+import Navigation from './Navigation';
+import User from './User';
+import Footer from './footer.js';
 
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 import './listing.css';
 
 class App extends Component {
   render() {
-    return <Listing />;
+    return (
+      <div>
+        <Listing />
+        <Navigation />
+        <User />
+        <Footer />
+      </div>
+    );
   }
 }
 

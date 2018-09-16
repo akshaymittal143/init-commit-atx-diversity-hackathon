f400c9bd585a12c44d40ac9d7142113e5fd84d8c
Daniel Palacios <danielpalacios530@gmail.com>
2018-09-16 09:09:48 -0500

    navbar and footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Navigation from './Navigation';
+import IndexBody from './IndexBody';
+import Footer from './Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <IndexBody />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
